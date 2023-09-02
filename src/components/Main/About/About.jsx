7adaf631bc17d280842f5a7d02a0bcb47c38ddf0
naveenkumarbhat <naveenbhat1414@gmail.com>
2023-09-02 17:13:51 +0530

    first commit

---
+++ src/components/Main/About/About.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { AppWrap } from "../../../wrapper";
+import "./About.css";
+import { images } from "../../../constants";
+
+const aboutData = {
+  title: "Little Lemon",
+  subTitle: "Chicago",
+  description: `Welcome to Little Lemon's newest branch in the heart of Chicago! We're thrilled to bring our delicious and refreshing menu to this vibrant city. Situated in a prime location, our Chicago branch offers a unique dining experience with a cozy and modern ambiance. Explore our menu featuring a wide range of mouthwatering dishes and freshly squeezed lemonades. Whether you're a local or just passing through, we invite you to savor the flavors of Little Lemon and join us in making Chicago a little bit zestier.`,
+  image1: images.MarioandAdrianA,
+  image2: images.resturantChefB,
+};
+const About = () => {
+  return (
+    <div className="app_about-section">
+      <div className="app_about-description-box">
+        <h1 className="app__about-title">{aboutData.title}</h1>
+        <br />
+        <h4 className="app__about-subtitle">{aboutData.subTitle}</h4>
+        <br />
+        <p className="app__about-description">
+          {aboutData.description}
+          <br />
+          <br />
+        </p>
+      </div>
+
+      {/* <div className="app__about-image-box-section"> */}
+      <div className="app__about-image-holder">
+        <div
+          className="app__about-image-box img-box-1"
+          style={{
+            backgroundImage: `url(${aboutData.image2})`,
+          }}
+        />
+        <div
+          className="app__about-image-box img-box-2"
+          style={{
+            backgroundImage: `url(${aboutData.image1})`,
+          }}
+        />
+      </div>
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default AppWrap(About, "About", "app__about");
